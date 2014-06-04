9ee0643ef49eaac791defa3ece284fff853ae054
G A <gdur.mugen@googlemail.com>
2014-06-04 19:09:41 +0200

    - added new colors - refactored RGB class
    Signed-off-by: G A <gdur.mugen@googlemail.com>

--- src/main/java/berlin/iconn/projects/segmentation/RGB.java
+++ src/main/java/berlin/iconn/projects/segmentation/RGB.java
@@ -7,22 +7,31 @@ public class RGB {
     int r, g, b;
     float nr, ng, nb;
 
+    RGB(int rgb) {
+        r = (rgb >>> 16) & 0xff;
+        g = (rgb >>> 8) & 0xff;
+        b = (rgb >>> 0) & 0xff;
+
+        set();
+    }
+
+    public void set() {
+        nr = r / 255.0f;
+        ng = g / 255.0f;
+        nb = b / 255.0f;
+    }
+
     RGB(int rr, int gg, int bb) {
-        nr = rr / 255.0f;
-        ng = gg / 255.0f;
-        nb = bb / 255.0f;
         r = rr;
         g = gg;
         b = bb;
+        set();
     }
 
     RGB() {
-        nr = (float) Math.random();
-        ng = (float) Math.random();
-        nb = (float) Math.random();
-
-        r = (int) (nr * 255);
-        g = (int) (ng * 255);
-        b = (int) (nb * 255);
+        r = (int) (Math.random() * 255);
+        g = (int) (Math.random() * 255);
+        b = (int) (Math.random() * 255);
+        set();
     }
 }
